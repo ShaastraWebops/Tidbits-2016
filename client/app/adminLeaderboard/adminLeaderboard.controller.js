8c221fac390c5caf83c02934f850f046c72cfd57
chinnichaitanya <chchaitanya95@gmail.com>
2016-01-03 02:49:24 +0530

    updates

--- client/app/adminLeaderboard/adminLeaderboard.controller.js
+++ client/app/adminLeaderboard/adminLeaderboard.controller.js
@@ -1,7 +1,9 @@
 angular.module('tidbitsApp')
   .controller('AdminLeaderboardCtrl', function ($scope, $http) {
 
+  	$scope.totalRegistrations = 0;
     $http.get('/api/users/adminLeaders/').then(function (res) {
+      $scope.totalRegistrations = res.data.length;
       $scope.users = res.data;
     });
 
