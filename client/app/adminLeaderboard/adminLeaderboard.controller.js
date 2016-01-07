3944fbc5d8afb2771d948edf7e1621e3377b91df
chinnichaitanya <chchaitanya95@gmail.com>
2016-01-08 03:44:08 +0530

    add excel downloader

--- client/app/adminLeaderboard/adminLeaderboard.controller.js
+++ client/app/adminLeaderboard/adminLeaderboard.controller.js
@@ -1,10 +1,20 @@
 angular.module('tidbitsApp')
-  .controller('AdminLeaderboardCtrl', function ($scope, $http) {
+  .controller('AdminLeaderboardCtrl', function ($scope, $http, ExcelDownloader, $timeout) {
 
   	$scope.totalRegistrations = 0;
     $http.get('/api/users/adminLeaders/').then(function (res) {
       $scope.totalRegistrations = res.data.length;
       $scope.users = res.data;
     });
+
+    $scope.exportToExcel = function (tableId) {
+      $scope.exportHref = ExcelDownloader.tableToExcel(tableId, 'sheet name');
+      $timeout(function () {
+        var link = document.createElement('a');
+        link.download = "Tidbits2016-AdminLeaderboard.xlsx";
+        link.href = $scope.exportHref;
+        link.click();
+      }, 100);
+    };
 
   });
